c0a12595851467ac9f0e529eae55f37bbec4a320
Dounia <saibdounia21@gmail.com>
2024-05-04 14:17:42 +0200

    add admin username and password

--- skeleton-web-app-school-main-front-skeleton/front-skeleton/src/app/admin/admin.component.ts
+++ skeleton-web-app-school-main-front-skeleton/front-skeleton/src/app/admin/admin.component.ts
@@ -1,12 +1,29 @@
-import { Component, OnInit } from "@angular/core"
+import { Component } from '@angular/core';
+import { AdminService } from 'services/admin.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: "admin",
-  templateUrl: "./admin.component.html",
-  styleUrls: ["./admin.component.scss"],
+  selector: 'admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.scss']
 })
-export class AdminComponent implements OnInit {
-  constructor() {}
+export class AdminComponent {
 
-  ngOnInit(): void {}
+  constructor(private adminService: AdminService, private router: Router) { }
+
+  login(username: string, password: string): void {
+    this.adminService.login(username, password)
+      .subscribe(
+        (response) => {
+          // Handle successful login response
+          console.log('Login successful:', response);
+          this.router.navigate(['home/home.component']); // Replace '/dashboard' with the desired route
+        },
+        (error) => {
+          // Handle login error
+          console.error('Login error:', error);
+          // Display error message to the user
+        }
+      );
+  }
 }
