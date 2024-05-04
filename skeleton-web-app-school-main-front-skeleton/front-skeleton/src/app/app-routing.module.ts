003bc5d8c4a7d6071316d0e9e1803f979560e245
Dounia <saibdounia21@gmail.com>
2024-05-04 15:15:15 +0200

    changes of front in main

--- skeleton-web-app-school-main-front-skeleton/front-skeleton/src/app/app-routing.module.ts
+++ skeleton-web-app-school-main-front-skeleton/front-skeleton/src/app/app-routing.module.ts
@@ -1,69 +1,10 @@
-import { NgModule } from "@angular/core"
-import { RouterModule, Routes } from "@angular/router"
-import { HomeComponent } from "home/home.component"
-import { StudentsComponent } from "students/students.component"
-import { StudentsResolver } from "students/students.resolver"
-import { StudentDetailsComponent } from "students/student-details/student-details.component"
-import { StudentDetailsResolver } from "students/student-details/student-details.resolver"
-import { MajorsComponent } from "majors/majors.component"
-import { MajorsResolver } from "majors/majors.resolver"
-import { MajorStudentsResolver } from "majors/major-students/major-students.resolver"
-import { MajorStudentsComponent } from "majors/major-students/major-students.component"
-import { AdminComponent } from "admin/admin.component"
-import { MathComponent } from 'math/math.component';
-import { HistoireComponent } from 'histoire/histoire.component';
-import { PhysiqueComponent } from 'physique/physique.component';
+import { Component } from "@angular/core"
 
-const routes: Routes = [
-  { path: "", component: HomeComponent },
-  {
-    path: "admin",
-    component: AdminComponent
-  },
-  {
-    path: "math",
-    component: MathComponent
-  },
-  {
-      path: "histoire",
-      component: HistoireComponent
-  },
-  {
-        path: "physique",
-        component: PhysiqueComponent
-    },
-  {
-    path: "etudiants",
-    component: StudentsComponent,
-    resolve: {
-      students: StudentsResolver,
-    },
-  },
-  {
-    path: "details-etudiant/:id",
-    component: StudentDetailsComponent,
-    resolve: {
-      student: StudentDetailsResolver,
-    },
-  },
-  {
-    path: "filieres",
-    component: MajorsComponent,
-    resolve: {
-      majors: MajorsResolver,
-    },
-  },
-  {
-    path: "etudiants-filiere/:id",
-    component: MajorStudentsComponent,
-    resolve: {
-      studentsFromMajor: MajorStudentsResolver,
-    },
-  },
-]
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+@Component({
+  selector: "root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppRoutingModule {}
+export class AppComponent {
+  title = "front-skeleton"
+}
